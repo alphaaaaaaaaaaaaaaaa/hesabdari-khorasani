20f4a9031ae97b0b035068ce7fd3ea335c772a6b
test <test@test.com>
2024-05-17 02:39:37 +0330

    first commit

---
+++ hesabdari/hesabdari.App/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Accounting.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton btnCustomers;
+        private System.Windows.Forms.ToolStripButton btnNewAccounting;
+        private System.Windows.Forms.ToolStripButton btnReportPay;
+        private System.Windows.Forms.ToolStripButton btnReportRecive;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDate;
+        private System.Windows.Forms.ToolStripStatusLabel lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem btnEditLogin;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblAccountBalance;
+        private System.Windows.Forms.Label lblPay;
+        private System.Windows.Forms.Label lblRecive;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ hesabdari/hesabdari.DataLayer/Services/GenericRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Context;
+using Accounting.DataLayer;
+
+namespace Accounting.DataLayer.Services
+{
+    public class GenericRepository<TEntity> : IDisposable where TEntity : class
+    {
+        private readonly Accounting_DBEntities _db;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(Accounting_DBEntities db)
+        {
+            _db = db;
+            _dbSet = _db.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> where = null)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (where != null)
+            {
+                query = query.Where(where);
+            }
+
+            return query.ToList();
+        }
+
+        public virtual TEntity GetById(object Id)
+        {
+            return _dbSet.Find(Id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _db.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public virtual void Delete(object Id)
+        {
+            var entity = GetById(Id);
+            Delete(entity);
+        }
+
+        public virtual void SaveChanges()
+        {
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                // Log the error with more detail
+                Console.WriteLine($"Error saving changes to the database: {ex.Message}");
+                throw; // re-throw the original exception
+            }
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
